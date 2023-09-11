b1a9f8b025d2c4cf531fce7a9683ddf25654e1dc
Jacob Hrbek <kreyren@rixotstudio.cz>
2023-09-11 19:33:08 +0200

    Firmware: Fix 404

--- pkgs/firmware/light_aon_fpga.nix
+++ pkgs/firmware/light_aon_fpga.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   version = "unstable-2023-06-25";
 
   src = fetchurl {
-    url = "https://github.com/chainsx/fedora-riscv-builder/raw/20230623-0255/firmware/light_aon_fpga.bin";
+    url = "https://github.com/chainsx/fedora-riscv-builder/raw/f35095c9f45b42addb1b0d0e9185ca8ff04ed870/firmware/light_aon_fpga.bin";
     hash = "sha256-cIFaWDbiX0nNzlhkhhKapjGeN64dblFE1DxZh01kV5s=";
   };
 
--- pkgs/firmware/light_c906_audio.nix
+++ pkgs/firmware/light_c906_audio.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   version = "unstable-2023-06-25";
 
   src = fetchurl {
-    url = "https://github.com/chainsx/fedora-riscv-builder/raw/20230623-0255/firmware/light_c906_audio.bin";
+    url = "https://github.com/chainsx/fedora-riscv-builder/raw/f35095c9f45b42addb1b0d0e9185ca8ff04ed870/firmware/light_c906_audio.bin";
     hash = "sha256-hv9OQhc6cH7swqVrhPhIxgUsgH4JMiOjw+DRWcK9nNQ=";
   };
 
